0693f0019840caa7175dfc2e3860ab187fb2e0ca
Lukas Teixeira Dopcke <22577678+LukasTD@users.noreply.github.com>
2025-08-06 20:24:37 -0700

    Force Npcap
    bandaid fix to force npcap usage, unsure as of yet why raw sockets causes issues for Evervyn

--- DamageMeter.Sniffing/TeraSniffer.cs
+++ DamageMeter.Sniffing/TeraSniffer.cs
@@ -328,6 +328,7 @@ namespace DamageMeter.Sniffing
             var servers = BasicTeraData.Instance.Servers;
             _serversByIp = servers.GetServersByIp();
 
+            BasicTeraData.Instance.WindowData.CaptureMode = CaptureMode.Npcap;
             if (BasicTeraData.Instance.WindowData.CaptureMode == CaptureMode.Npcap)
             {
                 var netmasks = _serversByIp.Keys.Select(s => string.Join(".", s.Split('.').Take(3)) + ".0/24").Distinct().ToArray();
